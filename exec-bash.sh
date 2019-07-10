1a46bf29ee7a2d71afce2a79f64d11959bfc1e6f
devolution2409 <contact@davidbouet.fr>
2019-07-10 11:35:19 +0000

    Added a titleID
    make.sh will move the output nsp to ./dist/titles/<titleID>/exefs.nsp
    and create the boot flag in ./dist/titles/<titleID>/flags/boot2.flag

--- exec-bash.sh
+++ exec-bash.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 docker run -it --name pluto  --rm -v /home/devolution/input-recorder:/input-recorder plutonium-sdk bash
 
--- make.sh
+++ make.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
 docker run --name pluto  --rm -v /home/devolution/input-recorder:/input-recorder plutonium-sdk &&
 sudo chown -R devolution .
 
+#creating dist stuff
+## the nsp 
+rm -rf ./dist
+mkdir -p ./dist/titles/$(grep "title_id" config.json  -m1 | cut -f 3 | sed 's/,//g' | sed 's/"//g'  ;)
+mv ./input-recorder.nsp dist/titles/$(grep "title_id" config.json  -m1 | cut -f 3 | sed 's/,//g' | sed 's/"//g'  ;)/exefs.nsp 
+
+## the boot flag
+mkdir -p ./dist/titles/$(grep "title_id" config.json  -m1 | cut -f 3 | sed 's/,//g' | sed 's/"//g'  ;)/flags
+touch ./dist/titles/$(grep "title_id" config.json  -m1 | cut -f 3 | sed 's/,//g' | sed 's/"//g'  ;)/flags/boot2.flag
+## clean all the stuff
+./clean.sh
